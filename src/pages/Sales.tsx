52212f18c3d0035a536b6db002f617b1142f7b18
mahmoud Attia <51999205+mamoda@users.noreply.github.com>
2025-09-15 22:36:42 +0300

    Update Sales.tsx

--- src/pages/Sales.tsx
+++ src/pages/Sales.tsx
@@ -61,7 +61,7 @@ const Sales: React.FC = () => {
 
         <div className="overflow-x-auto">
           <table className="min-w-full divide-y divide-gray-200">
-            <thead className="flex-row-reverse  bg-gray-50">
+            <thead className="bg-gray-50">
               <tr>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   تاريخ
